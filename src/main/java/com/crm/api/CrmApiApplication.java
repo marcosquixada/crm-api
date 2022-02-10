ef54627e1bf0e5f2dac7a2e3d00b7d56a53a47a5
Marcos Quixadá <marcos.quixada@yahoo.com.br>
2022-02-09 22:00:51 -0300

    docs: add open api 3 and descriptions

--- src/main/java/com/crm/api/CrmApiApplication.java
+++ src/main/java/com/crm/api/CrmApiApplication.java
@@ -1,13 +1,29 @@
 package com.crm.api;
-
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class CrmApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CrmApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CrmApiApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDesciption, @Value("${application-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(appDesciption)
+                        .version(appVersion)
+                        .description(appDesciption)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
 }
